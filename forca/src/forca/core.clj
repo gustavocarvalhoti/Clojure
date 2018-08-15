35c5afc8ccca9f6bd2511ccf25dd6f37e0612363
gusilva <gusilva@padtec.com.br>
2018-08-15 19:35:07 -0300

    15/08/2018 19:35

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -3,39 +3,51 @@
 
 (def total-de-vidas 6)
 
+(def palavra-secreta "MELANCIA")
+
 (defn perdeu [] (print "Você perdeu!"))
 
 (defn ganhou [] (print "Você ganhou!"))
 
 (defn letras-faltantes [palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra
-  )
+  (remove (fn [letra] (contains? acertos (str letra))) palavra)
 )
 
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos))
 )
 
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-      (perdeu)
-      (
-        do
-        (println vidas)
-        (if (acertou-a-palavra-toda? palavra acertos)
-            (ganhou)
-            (println "Chute o valor!")
-        )
-      )
-  )
-)
+  (cond
+   (= vidas 0) (perdeu)
+   (acertou-a-palavra-toda? palavra acertos) (ganhou)
+   :else
+   (let [chute (le-letra!)]
+     (if (acertou? chute palavra)
+       (do
+         (println "Acertou a letra!")
+         (recur vidas palavra (conj acertos chute)))
+       (do
+         (println "Errou a letra! Perdeu vida!")
+         (recur (dec vidas) palavra acertos))))))
+
+(defn start [] (jogo total-de-vidas palavra-secreta #{}))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (start))
 
 (defn compareNumeros [n1, n2]
   (if (= n1 n2)
     (println "Números iguais")
     (println "Números diferente")
+    )
   )
-)
 
 (defn fib[x]
   (if (= x 0) 0
@@ -55,8 +67,3 @@
 (defn utilizando-filter [numbers]
   (filter (fn [x] (or (< x 5) (> x 6))) numbers)
 )
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
